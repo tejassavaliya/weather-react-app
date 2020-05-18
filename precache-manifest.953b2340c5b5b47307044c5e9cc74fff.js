f3baac19c0f913e76888317c8f3a251ce7b4a3ee
tejas.savaliya <tejas14883@gmail.com>
2020-05-18 21:39:43 +0530

    Updates

---
+++ precache-manifest.953b2340c5b5b47307044c5e9cc74fff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4748b6641ba7909ba8fc8c3999ed0d6f",
+    "url": "/weather-react-app/index.html"
+  },
+  {
+    "revision": "25174dbc99c07e636ad1",
+    "url": "/weather-react-app/static/css/main.6da41fbb.chunk.css"
+  },
+  {
+    "revision": "8b928a238f88db4fa99a",
+    "url": "/weather-react-app/static/js/2.adc70d5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-react-app/static/js/2.adc70d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25174dbc99c07e636ad1",
+    "url": "/weather-react-app/static/js/main.ab82aa32.chunk.js"
+  },
+  {
+    "revision": "46441633ebef7d737ff0",
+    "url": "/weather-react-app/static/js/runtime-main.85527929.js"
+  }
+]);
